0e73ec12700d6c5664d90fab628939144e4a25fb
Roman <romario.higa@gmail.com>
2026-07-11 16:53:14 +0200

    json for Vercel-2

--- vite.config.js
+++ vite.config.js
@@ -6,37 +6,48 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: process.env.GITHUB_ACTIONS ? '/first-team-project/' : '/',
+
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
+
     root: 'src',
+
     build: {
       sourcemap: true,
       rollupOptions: {
         input: glob.sync('./src/*.html'),
+
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
               return 'vendor';
             }
           },
+
           entryFileNames: chunkInfo => {
             if (chunkInfo.name === 'commonHelpers') {
               return 'commonHelpers.js';
             }
+
             return '[name].js';
           },
+
           assetFileNames: assetInfo => {
             if (assetInfo.name && assetInfo.name.endsWith('.html')) {
               return '[name].[ext]';
             }
+
             return 'assets/[name]-[hash][extname]';
           },
         },
       },
+
       outDir: '../dist',
       emptyOutDir: true,
     },
+
     plugins: [
       injectHTML(),
       FullReload(['./src/**/**.html']),
